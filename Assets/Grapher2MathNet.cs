26585b975d2d617d96973499ff23441760eb9954
Zoe McCarthy <zoemccarthy12@gmail.com>
2014-10-11 22:33:07 -0700

    got mathnet to work, wrote QuadraticForm function delegate using mathnet, appears to work

---
+++ Assets/Grapher2MathNet.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics;
+
+public class Grapher2MathNet : MonoBehaviour {
+	
+	public enum FunctionOption {
+		Linear,
+		Exponential,
+		Parabola,
+		Sine,
+		Ripple,
+		QuadraticForm
+	}
+	
+	private delegate float FunctionDelegate (Vector3 p, float t);
+	private static FunctionDelegate[] functionDelegates = {
+		Linear,
+		Exponential,
+		Parabola,
+		Sine,
+		Ripple,
+		QuadraticForm
+	};
+	
+	public FunctionOption function;
+	
+	[Range(10, 100)]
+	public int resolution = 10;
+	
+	private int currentResolution;
+	private ParticleSystem.Particle[] gridPoints;
+	
+	private void CreatePoints () {
+		currentResolution = resolution;
+		gridPoints = new ParticleSystem.Particle[resolution * resolution];
+		float increment = 1f / (resolution - 1);
+		int i = 0;
+		for (int x = 0; x < resolution; x++) {
+			for (int z = 0; z < resolution; z++) {
+				Vector3 p = new Vector3(increment * (x - resolution / 2), 0f, increment * (z - resolution / 2));
+				gridPoints[i].position = p;
+				gridPoints[i].color = new Color(p.x + increment * resolution / 2, 0f, p.z + increment * resolution / 2);
+				gridPoints[i++].size = 0.1f;
+			}
+		}
+	}
+	
+	void Update () {
+		if (currentResolution != resolution || gridPoints == null) {
+			CreatePoints();
+		}
+		FunctionDelegate f = functionDelegates[(int)function];
+		float t = Time.timeSinceLevelLoad;
+		for (int i = 0; i < gridPoints.Length; i++) {
+			Vector3 p = gridPoints[i].position;
+			p.y = f(p, t);
+			gridPoints[i].position = p;
+			Color c = gridPoints[i].color;
+			c.g = p.y;
+			gridPoints[i].color = c;
+		}
+		particleSystem.SetParticles(gridPoints, gridPoints.Length);
+
+
+		Matrix m = new Matrix(new double[][] {
+			new double[] { 10.0, -18.0 },
+			new double[] { 6.0, -11.0 } });
+		
+		// alternative way to create the matrix:
+		// double[][] data = Matrix.CreateMatrixData(2, 2);
+		// data[0][0] = 10.0;
+		// data[1][0] = 6.0;
+		// data[0][1] = -18.0;
+		// data[1][1] = -11.0;
+		// Matrix m = new Matrix(data);
+		
+		//EigenvalueDecomposition eigen = m.EigenvalueDecomposition;
+		
+		//Complex[] eigenValues = eigen.EigenValues;
+		// eigenvalues: 1, -2
+		
+		//Matrix eigenVectors = eigen.EigenVectors;
+		
+		// eigenvectors: [0.894...,0.447...] and [6.708...,4.473...]
+		
+		// alternative way to access the eigenvalues witout the Complex type:
+		// double[] eigenValuesReal = eigen.RealEigenvalues; // real part
+		// double[] eigenValuesImag = eigen.ImagEigenvalues; // imaginary part
+	}
+	
+	private static float Linear (Vector3 p, float t) {
+		return p.x;
+	}
+	
+	private static float Exponential (Vector3 p, float t) {
+		return p.x * p.x;
+	}
+	
+	private static float Parabola (Vector3 p, float t){
+		return 1f - p.x * p.x * p.z * p.z;
+	}
+
+	private static float QuadraticForm (Vector3 p, float t){
+		double x =  (double) Mathf.Sin (0.5f * t + .1f);
+		double z =  (double) Mathf.Cos(0.5f * t);
+		double xz = (double) 0.1f * Mathf.Sin (0.25f * t);
+		Matrix a = new Matrix(new double[][] {
+			new double[] { x, xz },
+			new double[] { xz, -z } });
+		Matrix v = new Matrix(new double[][] {
+			new double[] {p.x},
+			new double[] {p.z}});
+		Matrix vt = v.Clone();
+		vt.Transpose();
+		Matrix v3 = vt * a * v;
+		return (float) v3.GetArray()[0][0];
+	}
+	
+	private static float Sine (Vector3 p, float t){
+		return 0.50f +
+			0.25f * Mathf.Sin(4 * Mathf.PI * p.x + 4 * t) * Mathf.Sin(2 * Mathf.PI * p.z + t) +
+				0.10f * Mathf.Cos(3 * Mathf.PI * p.x + 5 * t) * Mathf.Cos(5 * Mathf.PI * p.z + 3 * t) +
+				0.15f * Mathf.Sin(Mathf.PI * p.x + 0.6f * t);
+	}
+	
+	private static float Ripple (Vector3 p, float t){
+		p.x -= 0.5f;
+		p.z -= 0.5f;
+		float squareRadius = p.x * p.x + p.z * p.z;
+		return 0.5f + Mathf.Sin(15f * Mathf.PI * squareRadius - 2f * t) / (2f + 100f * squareRadius);
+	}
+}
